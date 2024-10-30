8288e2936a71fa4c748d626d2eafaf695b078de3
Adam Measom <adameasom@mail.com>
2024-10-30 09:43:16 +0900

    change arrow to icon

--- src/components/EvolutionPath.jsx
+++ src/components/EvolutionPath.jsx
@@ -92,7 +92,11 @@ const EvolutionPath = ({ pokemonId, onPokemonClick }) => {
                     <p><span>#</span>{evolution.id.toString().padStart(3, '0')}</p>
                     <p>{evolution.name.charAt(0).toUpperCase() + evolution.name.slice(1)}</p>
                   </div>
-                  {index < evolutions.length - 1 && <div className="arrow right-arrow"></div>}
+                  {index < evolutions.length - 1 && <div className="arrow">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                      <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                    </svg>
+                  </div>}
                 </React.Fragment>
               ))}
             </div>
