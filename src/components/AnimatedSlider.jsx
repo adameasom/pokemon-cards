24041fb787700b14ce5cf11e5a471a60d5ab7dd6
Adam Measom <adameasom@mail.com>
2024-11-01 00:00:24 +0900

    Increase icon size

--- src/components/AnimatedSlider.jsx
+++ src/components/AnimatedSlider.jsx
@@ -259,12 +259,12 @@ const AnimatedSlider = ({ items, onFilterType, onActiveChange, onPokemonClick, a
       </div>
       <div className="buttons">
         <button id="prev" className="ArrowLeft" onClick={prev} style={{ visibility: active > 0 ? 'visible' : 'hidden' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" className="size-6">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="currentColor" className="size-6">
             <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
           </svg>
         </button>
         <button id="next" className="ArrowRight" onClick={next} style={{ visibility: active < items.length - 1 ? 'visible' : 'hidden' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" className="size-6">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="currentColor" className="size-6">
             <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
           </svg>
         </button>
